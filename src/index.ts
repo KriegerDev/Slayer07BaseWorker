262914a90706d40fd03b2a5e8dfbf28b76212c39
KriegerDev <lorenzogabrielwalecheski@gmail.com>
2023-10-22 02:36:54 -0300

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import config from "./clientcfg.json";
 import Slayer from 'slayer07';
 
-const worker = new Slayer.SlayerWorker(config.workerId, config.clientToken, config.clientId);
+const worker = new Slayer(config.workerId, config.clientToken, config.clientId);
 worker.login(worker.worker_token);
 
 worker.once('ready', () => {
